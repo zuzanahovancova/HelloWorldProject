18eeb1644c43d1b2ed46b2f9ab2781f2a101783c
Zuzana <zuzanahovanec@gmail.com>
2023-01-20 21:05:06 +0100

    creating new methods in the class ExcelBasicFormulas

--- src/basicexamples/exercises/ExcelBasicFormulas.java
+++ src/basicexamples/exercises/ExcelBasicFormulas.java
@@ -1,31 +1,87 @@
 package basicexamples.exercises;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ExcelBasicFormulas {
+
+    public static double getSum(double[] values) {
+        //tu musi byt logika vypoctu
+        double sum = 0;
+        for (int i = 0; i < values.length; i++) { // cyklus s pevnym poctom opakovani, i nadobuda hodnoty od 0 do 3
+            sum = sum + values[i]; // Java interpretuje tento riadok najprv tak, ze zoberie alebo vypocita to, co je napravo od =
+        }
+        return sum;
+    }
+
+    public static double getAverage(double sum, double count) {
+
+        double average = sum / count;
+        return average;
+    }
+
+    /* 2.SPOSOB
+    public static double getAverageFromArray (double [] values) {
+       double sum = getSum(values);
+       int count = values.length;
+       double average = sum/count;
+       return average;
+     */
+    /* 3.SPOSOB
+    public static double DOPISAT!!*/
+    public static double getMax(double[] values) {
+
+        Arrays.sort(values); // pole values je zoradene
+        double max = values[values.length - 1];
+        return max;
+    }
+
+    public static double getMin(double[] values) {
+
+        Arrays.sort(values); // pole values je zoradene
+        double min = values[0];
+        return min;
+    }
+
+    public static double getCount(double[] values) {
+
+        double count = values.length;
+        return count;
+    }
+
     public static void main(String[] args) {
-        int[] values = {10, 20, 30, 40}; // [] pole,{}// hodnoty v poli
+
+        double[] values = {10, 20, 30, 40}; // [] pole,{}// hodnoty v poli
 
         //System.out.println(values[0]); //0 je index,ktory zacina 0...0 je prva hodnota v poli...1 je druha,atd.
 
-        int sum = 0;
+        double sum = getSum(values);
 
-        for (int i = 0; i < 4; i++) {// do premennej i sme dali 0,aby sme vybrali prvy prvok v poli...cyklus s pevnym poctom opakovani, i nadobuda hodnoty od 0 do 4
-            // i++ hodnota i zvacsena o 1,akoby i= i+1
-            sum = sum + values[i];
-            // v 1.cykle(iteracii) bude sum obsahovať hodnotu 10,pretože 0+10
-            // v 2.cykle bude sum obsahovať hodnotu 30, pretože 10 + 20
-            // v 3.cykle bude sum obsahovať hodnotu 60,pretože 30 + 30
-            // v 4.cykle -II- 60+40
+        double count = values.length;
+        double average = getAverage(sum, count);
 
-        }
-        System.out.println("SUM" + " " + sum);
+        //v premennej sum dostanem posledny sucet
+        System.out.println("SUM" + " " + getSum(values));
+        System.out.println("AVERAGE" + " " + getAverage(sum, count));
+        System.out.println("COUNT" + " " + getCount(values));
 
-        for (int i = 0; i < values.length; i++) {
-            sum = sum + values[i];
-        }
-        System.out.println(sum);
+        // Ako vypocitat MAX
 
-        double average = sum / values.length;
-        System.out.println("AVERAGE" + " " + average);
+        System.out.println("MAX" + " " + getMax(values));
+        System.out.println("MIN" + " " + getMin(values));
+
+    }
+
+    @Test
+    public void testGetSum() {
+        double [] values = {1,2,3}; //naplnene pole s nazvom values hodnotami
+        assertEquals(6,getSum(values));
+
+        double [] values2 = {3,8,10,100};
+        assertEquals(121, getSum(values2));
     }
 
 }
--- src/basicexamples/test/ArrayExample.java
+++ src/basicexamples/test/ArrayExample.java
@@ -1,5 +1,7 @@
 package basicexamples.test;
 
+import java.util.Arrays;
+
 public class ArrayExample {
 
     public static void main(String[] args) {
@@ -13,5 +15,34 @@ public class ArrayExample {
             System.out.println(values[i]); //vypiseme obsah pola prostrednictvom kombinacie nazovPola[i]
 
         }
+        int[] array = new int[10];
+
+        //naplnime kazdy prvok pola nahodnou hondnotou od 1 do 100
+        array[0] = ((int) (Math.random() * 100 + 1));
+        array[1] = ((int) (Math.random() * 100 + 1));
+        array[2] = ((int) (Math.random() * 100 + 1));
+        array[3] = ((int) (Math.random() * 100 + 1));
+        array[4] = ((int) (Math.random() * 100 + 1));
+        array[5] = ((int) (Math.random() * 100 + 1));
+        array[6] = ((int) (Math.random() * 100 + 1));
+        array[7] = ((int) (Math.random() * 100 + 1));
+        array[8] = ((int) (Math.random() * 100 + 1));
+        array[9] = ((int) (Math.random() * 100 + 1));
+
+        //tu vypise kazdy prvok pola
+        System.out.println(array[0] + " " + array[1] + " " + array[2] + " " + array[3]
+                + " " + array[4] + " " + array[5] + " " + array[6] + " " + array[7] + " "
+                + array[8] + " " + array[9]);
+
+        Arrays.sort(array); //zoradi pole od najmensieho po najvacsie
+        //System.out.println(array);//zobrazit takymto sposobom nepojde
+
+        System.out.println("Zoradené údaje (od najmenšieho po najväčšie)");
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+        System.out.println("\nNajväčšie číslo je:" + array[array.length-1]);
+        System.out.println("Najmenšie číslo je:" + array[0]);
     }
 }
